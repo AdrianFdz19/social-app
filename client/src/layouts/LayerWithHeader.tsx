a3375887774f3ccf10c096b2380f65ce29fd76c5
AdrianFdz19 <126997909+AdrianFdz19@users.noreply.github.com>
2024-12-17 14:13:44 -0600

    feature: create post modal and useUpload hook

--- client/src/layouts/LayerWithHeader.tsx
+++ client/src/layouts/LayerWithHeader.tsx
@@ -1,7 +1,7 @@
 // Header.tsx
 
 import { Outlet } from "react-router-dom";
-import Header from "../components/header/Header";
+import Header from "../components/header/Header.tsx";
 
 export default function LayerWithHeader() {
   return (
